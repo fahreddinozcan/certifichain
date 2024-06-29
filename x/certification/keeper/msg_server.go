d236408666fb749d8d7eda429b3c103f9189e275
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-06-29 18:06:55 +0200

    add rest of keepers and types

--- x/certification/keeper/msg_server.go
+++ x/certification/keeper/msg_server.go
@@ -2,6 +2,9 @@ package keeper
 
 import (
 	"certifichain/x/certification/types"
+	"context"
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 type msgServer struct {
@@ -15,3 +18,40 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
+
+// IssueCertification handles the MsgIssueCertification message
+func (ms msgServer) IssueCertification(goCtx context.Context, msg *types.MsgIssueCertification) (*types.MsgIssueCertificationResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Create a new Certification object
+	cert := &types.Certification{
+		Id:          msg.Id,
+		IssuerId:    msg.IssuerId,
+		LearnerId:   msg.LearnerId,
+		Title:       msg.Title,
+		Description: msg.Description,
+		IssueDate:   msg.IssueDate,
+		ExpiryDate:  msg.ExpiryDate,
+		Skills:      msg.Skills,
+		Hash:        msg.Hash,
+		Creator:     msg.Creator,
+	}
+
+	// Call the keeper method to issue the certification
+	err := ms.Keeper.IssueCertification(ctx, *cert)
+	if err != nil {
+		return nil, fmt.Errorf("failed to issue certification: %w", err)
+	}
+
+	// Emit an event
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeIssueCertification,
+			sdk.NewAttribute(types.AttributeKeyCertificationId, cert.Id),
+			sdk.NewAttribute(types.AttributeKeyIssuerId, cert.IssuerId),
+			sdk.NewAttribute(types.AttributeKeyLearnerId, cert.LearnerId),
+		),
+	)
+
+	return &types.MsgIssueCertificationResponse{}, nil
+}
