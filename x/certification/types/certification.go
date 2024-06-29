90adb2686fbc58d7f1727a60d8ba5579b7fcd794
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-06-29 17:44:01 +0200

    certification proto interface

--- x/certification/types/certification.go
+++ x/certification/types/certification.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Certification struct {
 	Id          string
 	IssuerId    string
@@ -12,3 +17,26 @@ type Certification struct {
 	Hash        string
 	Creator     string
 }
+
+func (c *Certification) Reset() {
+	*c = Certification{}
+}
+
+func (c *Certification) String() string {
+	return fmt.Sprintf(`Certification:
+    ID: %s
+    Issuer ID: %s
+    Learner ID: %s
+    Title: %s
+    Description: %s
+    Issue Date: %d
+    Expiry Date: %d
+    Skills: %s
+    Hash: %s
+    Creator: %s`,
+		c.Id, c.IssuerId, c.LearnerId, c.Title, c.Description,
+		c.IssueDate, c.ExpiryDate, strings.Join(c.Skills, ", "),
+		c.Hash, c.Creator)
+}
+
+func (*Certification) ProtoMessage() {}
